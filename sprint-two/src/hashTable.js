90a5b51be921571632c04f6899c4b841763e7606
maspen <mattaspen@gmail.com>
2018-04-27 19:33:14 -0700

    started to re-implement hashTable to use tuples

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,24 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+// get at index & see if undefined
+  if(this._storage.get(index) === undefined) {
+    this._storage.set(index, []);
+  }
+  var tupleArrayAtIndex = this._storage.get(index);
+  tupleArrayAtIndex.push([k,v]);
+    
+  // this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.delete(index);
 };
 
 
