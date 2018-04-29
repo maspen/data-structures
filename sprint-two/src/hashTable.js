367ad90da6cea2844550b6017b0ec1a036ca21c8
maspen <mattaspen@gmail.com>
2018-04-28 17:38:14 -0700

    attempted more difficult problem increasing/decreasing HT size

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,11 +3,14 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._numberInserts = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-// get at index & see if undefined
+  
+  console.log('index ' + index);
+
   if(this._storage.get(index) === undefined) {
     this._storage.set(index, []);
   }
@@ -17,11 +20,33 @@ HashTable.prototype.insert = function(k, v) {
     if (tupleArrayAtIndex[i][0] === k) {
       tupleArrayAtIndex[i] = [k, v];
       !replaced;
+      // increment number of tuple inserts so can
+      // later use to determine if _limit needs to increase
+      this._numberInserts++;
     }
   }
   if (!replaced) {
     tupleArrayAtIndex.push([k, v]);
+    this._numberInserts++;
   }
+  // debugger;
+  // increase limit if 75%
+  // var numberOccupiedArrayCells = 0;
+  // for(var l = 0; l < this._limit; l++) {
+  //   // 
+  //   if(this._storage.get(l) !== undefined) {
+  //     numberOccupiedArrayCells++;
+  //   }
+  // }
+  if(this._numberInserts >= (this._limit * 0.75)) {
+    this._limit = this._limit * 2;
+  }
+  console.log(this._numberInserts);
+  console.log(this._limit);
+};
+
+HashTable.prototype.changeLimit = function(newLimit) {
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
